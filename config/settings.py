6a44b604facff9444917d94d7b46c4129b6ba12b
Fran Alonso <fraloal97@gmail.com>
2021-02-25 15:10:12 +0100

    django heroku

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,4 +145,3 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID= 1
 
-django_heroku.settings(locals())

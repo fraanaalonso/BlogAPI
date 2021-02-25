f58d03df139f55c298316589ec6ab15a1d8c244c
Fran Alonso <fraloal97@gmail.com>
2021-02-25 15:34:38 +0100

    allowed hosts: herokuapp

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'zrc$lmh5gm0-2a7l=5m0n=l#&@jhaaxfj%str1bsd%f+oak(^o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
